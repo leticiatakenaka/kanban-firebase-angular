728a0eebe4f0bb1fe5577529fca90c5393ec8526
leticiatakenaka <leticiatakenaka12@gmail.com>
2022-08-20 08:45:20 -0300

    Projeto Finalizado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfireangular',
+    appId: '1:334017488967:web:188ab8c3fdfa52c182a962',
+    storageBucket: 'kanbanfireangular.appspot.com',
+    apiKey: 'AIzaSyBhuZOEb-foWQLvJikoNb35bTivVEeWOLE',
+    authDomain: 'kanbanfireangular.firebaseapp.com',
+    messagingSenderId: '334017488967',
+    measurementId: 'G-Q3QV8791NY',
+  },
+  production: true
+};
